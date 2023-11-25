3cd75fd4e5ccc35c6c9dcde7f51a32294097871e
AylaDursun <dursun.a@northeastern.edu>
2023-11-25 12:18:36 -0500

    cell ref tests

--- implementation/spreadsheet/src/models/tests/strategy-cell-ref.spec.ts
+++ implementation/spreadsheet/src/models/tests/strategy-cell-ref.spec.ts
@@ -1,8 +1,48 @@
 import { assert } from "console";
 import { PlusSignStrategy } from "../strategy-plus-sign";
+import { CellRefStrategy } from "../strategy-cell-ref";
+import { Cell } from "../cell";
 describe('Cell Ref Strategy', (): void => {
-    let strategy = new PlusSignStrategy()
   
-    it('should concatenate 2 strings', (): void => {
+    it('should return the display value of a cell with a set value', (): void => {
+        let cells: Cell[][] = [];
+        let cell: Cell = new Cell(0, 0)
+        cell.setEnteredValue("string");
+        cell.updateDisplayValue(cells);
+        cells.push([cell]);
+        let strategy = new CellRefStrategy(cells);
+
+        expect(strategy.parse("REF(A1)")).toBe("string");
+    });
+
+    it('should return the display value of a cell with an enterred value that is different than the display value', (): void => {
+        let cells: Cell[][] = [];
+        let cell: Cell = new Cell(0, 0)
+        cell.setEnteredValue("1 + 2");
+        cell.updateDisplayValue(cells);
+        cells.push([cell]);
+        let strategy = new CellRefStrategy(cells);
+
+        expect(strategy.parse("REF(A1)")).toBe("3");
+    });
+
+    it('should return an empty string for a cell with no entered value', (): void => {
+        let cells: Cell[][] = [];
+        let cell: Cell = new Cell(0, 0);
+        cells.push([cell]);
+        let strategy = new CellRefStrategy(cells);
+
+        expect(strategy.parse("REF(A1)")).toBe("");
+    });
+
+    it('should return a simplified equation when the cell ref is not all that is present', (): void => {
+        let cells: Cell[][] = [];
+        let cell: Cell = new Cell(0, 0)
+        cell.setEnteredValue("1 + 2");
+        cell.updateDisplayValue(cells);
+        cells.push([cell]);
+        let strategy = new CellRefStrategy(cells);
+
+        expect(strategy.parse("1 + REF(A1)")).toBe("1 + 3");
     });
   });
